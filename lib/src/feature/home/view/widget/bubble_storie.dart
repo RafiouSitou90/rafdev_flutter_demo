a36f792ab2f1f55159543a8b93db2a897224302a
Rafiou Sitou <rafiousitou90@gmail.com>
2023-04-26 21:56:24 -0300

    feat(aula-kenniston): Add course content. 26-04-2023

---
+++ lib/src/feature/home/view/widget/bubble_storie.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+
+class BubbleStorie extends StatelessWidget {
+  const BubbleStorie(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.grey[400],
+            image: const DecorationImage(
+              image: Svg('lib/assets/images/user_placeholder.svg'),
+            ),
+          ),
+        ),
+        Text(text)
+      ],
+    );
+  }
+}
--- lib/src/feature/home/view/widget/home_widget.dart
+++ lib/src/feature/home/view/widget/home_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
+import 'package:rafdev/src/feature/home/view/widget/bubble_storie.dart';
 
 class UserHome extends StatefulWidget {
   const UserHome({Key? key}) : super(key: key);
@@ -9,7 +10,7 @@ class UserHome extends StatefulWidget {
   State<UserHome> createState() => _UserHomeState();
 }
 
-PreferredSizeWidget _homeAppBar() {
+PreferredSizeWidget _homeAppBar(BuildContext context) {
   return AppBar(
     title: Text(
       'home_title'.i18n(),
@@ -39,11 +40,28 @@ PreferredSizeWidget _homeAppBar() {
   );
 }
 
+Widget _stories(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 15.0),
+    child: SizedBox(
+      height: 100,
+      child: ListView.separated(
+        itemBuilder: (context, index) => BubbleStorie('name $index'),
+        separatorBuilder: (context, index) => const SizedBox(
+          width: 15.0,
+        ),
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+      ),
+    ),
+  );
+}
+
 class _UserHomeState extends State<UserHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _homeAppBar(),
+      appBar: _homeAppBar(context),
       body: Container(
         color: Colors.blue,
         child: const Center(
